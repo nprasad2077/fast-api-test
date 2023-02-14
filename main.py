7c8f3b5280241660f60e09bd31472793a382fa2e
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-13 21:35:32 -0600

    fast api

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: float
+
 
 app = FastAPI()
 
 @app.get("/")
-async def read_root():
-    return {'Hello': 'World'}
+async def root():
+    return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int,):
+    return {"item_id": item_id}
 
 
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
